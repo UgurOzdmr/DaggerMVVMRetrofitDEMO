817be2320303e4b2346e9b1aa00a3ddc18567567
uozdemir <redwatercc@gmail.com>
2020-09-30 11:24:24 +0300

    LiveData, reactive streams and subscribe & keep auth user.

--- app/src/main/java/com/example/daggermvvmretrofit/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/example/daggermvvmretrofit/ui/auth/AuthViewModel.java
@@ -1,26 +1,49 @@
 package com.example.daggermvvmretrofit.ui.auth;
 
-import android.util.Log;
-
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
+import com.example.daggermvvmretrofit.models.User;
 import com.example.daggermvvmretrofit.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
 
 public class AuthViewModel extends ViewModel {
     private static final String TAG = "AuthViewModel";
 
     private final AuthApi authApi;
 
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+
+
     @Inject
     public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
-        
-        if (this.authApi == null) {
-            Log.d(TAG, "AuthViewModel: auth api is null");
-        } else {
-            Log.d(TAG, "AuthViewModel: is not null");
-        }
+    }
+
+    public void authenticateWithId(int userId) {
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authApi.getUser(userId)
+                .subscribeOn(Schedulers.io())
+        );
+
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+    }
+
+    public LiveData<User> observeUser() {
+        return authUser;
     }
 }
